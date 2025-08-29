892d38c0670b9802974534b7cd8215a6ee5b12cf
hunainamalik651@gmail.com <hunainamalik651@gmail.com>
2025-08-29 10:21:12 -0700

    update

---
+++ chap1/Q1-Q5/app.js
@@ -0,0 +1,17 @@
+// Q1. Write a script to greet your website visitor using JS alert box.
+alert("Hello! Thanks for visiting my site.");
+
+// Q2. Write a script to display following message on your web page:
+alert("Error! Please enter a valid password.");
+
+// Q3. Write a script to display following message on your web page: (Hint : Use line break)
+alert("Welcome to JS Land...\nHappy Coding!");
+
+// Q4. Write a script to display following messages in sequence:
+alert("JavaScript Alert! Welcome to Js land...");
+
+alert("Happy Coding \n Prevents this page from creating additional dialogs")
+
+// Q5. Generate the following message through browser’s developer console:
+console.log(alert("Hello...I can run JS through myweb browser console"))
+
---
+++ chap1/Q6/app.js
@@ -0,0 +1,1 @@
+alert("This is an alert used in my HTML & CSS project!");
