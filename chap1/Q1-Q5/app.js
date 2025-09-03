23de1231ce22edc2e5738631ad6b811519a4f13f
hunainamalik651@gmail.com <hunainamalik651@gmail.com>
2025-09-03 07:11:43 -0700

    done

--- chap1/Q1-Q5/app.js
+++ chap1/Q1-Q5/app.js
@@ -2,16 +2,16 @@
 alert("Hello! Thanks for visiting my site.");
 
 // Q2. Write a script to display following message on your web page:
-alert("Error! Please enter a valid password.");
+// alert("Error! Please enter a valid password.");
 
 // Q3. Write a script to display following message on your web page: (Hint : Use line break)
-alert("Welcome to JS Land...\nHappy Coding!");
+// alert("Welcome to JS Land...\nHappy Coding!");
 
 // Q4. Write a script to display following messages in sequence:
-alert("JavaScript Alert! Welcome to Js land...");
+// alert("JavaScript Alert! Welcome to Js land...");
 
-alert("Happy Coding \n Prevents this page from creating additional dialogs")
+// alert("Happy Coding \n Prevents this page from creating additional dialogs")
 
 // Q5. Generate the following message through browser’s developer console:
-console.log(alert("Hello...I can run JS through myweb browser console"))
+// console.log(alert("Hello...I can run JS through myweb browser console"))
 
---
+++ chap2/app.js
@@ -0,0 +1,48 @@
+// Question 1
+// var usrname;
+
+// Question 2
+// var myName = "Hunaina Malik";
+// console.log(myName);
+
+// Question 3
+// var message = "Hello World";
+// alert(message);
+
+// Question 4
+// var studentName = "Jhone Doe";
+// alert(studentName);
+
+// var studentAge = "15 years old";
+// alert(studentAge);
+
+// var studentCourse = "Certified Mobile Application Development";
+// alert(studentCourse);
+
+// Question 5
+// var dish = `Pizza 
+//             Pizz
+//             Piz
+//             Pi
+//             P`;
+
+// alert(dish);
+
+// Question 6
+// var email = "example@example.com";
+// alert("My email address is " + email);
+
+// Question 7
+// var book = "A smarter way to learn JavaScript";
+// alert(book);
+
+// Question 8
+// document.write("Yah!I can write html content through javascript");
+
+// Question 9
+// var symbol = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬” "
+// alert(symbol)
+
+
+
+
