047eba77df338f83ba8ffe0f3a77d012feef9a3c
hunainamalik651@gmail.com <hunainamalik651@gmail.com>
2025-09-04 10:20:14 -0700

    done

--- chap 3/app.js
+++ chap 3/app.js
@@ -1,22 +1,22 @@
 // Question 1
-// var age = 19;
-// alert("I am " +age +" years old");
+// let age = 19;
+// alert("I am " + age + " years old");
 
 // Question 2
-// var visitNumber=14;
-// alert("You have visited this site "+visitNumber+" times");
+// let visitNumber = 14;
+// alert("You have visited this site " + visitNumber + " times");
 
 // Question 3
-//  var birthYear = 2006;
-//     document.write("My birth year is " + birthYear + "<br>");
-//     document.write("Data type of my declared variable is " + typeof birthYear);
+// let birthYear = 2006;
+// document.write("My birth year is " + birthYear + "<br>");
+// document.write("Data type of my declared variable is " + typeof birthYear);
 
 // Question 4
-// var visitorName= "John Doe";
-// var productTitle= "T-Shirts";
-// var quantity=5;
+// let visitorName = "John Doe";
+// let productTitle = "T-Shirts";
+// let quantity = 5;
 
-// document.write("<b>John Doe</b> ordered <b>5 T-shirt(s)</b> on XYZ Clothing store");
+// document.write("<b>" + visitorName + "</b> ordered <b>" + quantity + " " + productTitle + "(s)</b> on XYZ Clothing store");
 
 
 
--- chap 4/app.js
+++ chap 4/app.js
@@ -1,21 +1,21 @@
 // Question 1
-// var statement = "abc,xyz,123";
+// let statement = "abc,xyz,123";
 // console.log(statement);
 
 // Question 2
 // Legal variable names
-// var userName;
-// var my_age;
-// var $dollarRate;
-// var totalMarks;
-// var _studentID;
+// let userName;
+// let my_age;
+// let $dollarRate;
+// let totalMarks;
+// let _studentID;
 
 // Illegal variable names
-// var 123name = "Ahmed";
-// var my name = "Sara";
-// var var = "test";
-// var user-name = "Naina";
-// var alert = "hello";
+// let 123name = "Ahmed";
+// let my name = "Sara";
+// let let = "test";
+// let user-name = "Naina";
+// let alert = "hello";
 
 // Question 3
 document.write("<h2>Rules for naming JS variables</h2> <br>Variable names can only contain numbers,$ and -For example $my_1stVariable </br> Variables must begin with a letter,- or $. For example $name, _name or name </br>Variable names are case sensitive </br> Variable names should not be JS keywords")
