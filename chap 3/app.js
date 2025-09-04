1e65b0d43140e565c6cd7a3bb1d1537846b6725f
hunainamalik651@gmail.com <hunainamalik651@gmail.com>
2025-09-04 09:55:00 -0700

    done

---
+++ chap 3/app.js
@@ -0,0 +1,23 @@
+// Question 1
+// var age = 19;
+// alert("I am " +age +" years old");
+
+// Question 2
+// var visitNumber=14;
+// alert("You have visited this site "+visitNumber+" times");
+
+// Question 3
+//  var birthYear = 2006;
+//     document.write("My birth year is " + birthYear + "<br>");
+//     document.write("Data type of my declared variable is " + typeof birthYear);
+
+// Question 4
+// var visitorName= "John Doe";
+// var productTitle= "T-Shirts";
+// var quantity=5;
+
+// document.write("<b>John Doe</b> ordered <b>5 T-shirt(s)</b> on XYZ Clothing store");
+
+
+
+
