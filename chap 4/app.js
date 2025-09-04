db8985a036207ccf8c15b5edd6c94e961810d497
hunainamalik651@gmail.com <hunainamalik651@gmail.com>
2025-09-04 10:16:51 -0700

    done

---
+++ chap 4/app.js
@@ -0,0 +1,21 @@
+// Question 1
+// var statement = "abc,xyz,123";
+// console.log(statement);
+
+// Question 2
+// Legal variable names
+// var userName;
+// var my_age;
+// var $dollarRate;
+// var totalMarks;
+// var _studentID;
+
+// Illegal variable names
+// var 123name = "Ahmed";
+// var my name = "Sara";
+// var var = "test";
+// var user-name = "Naina";
+// var alert = "hello";
+
+// Question 3
+document.write("<h2>Rules for naming JS variables</h2> <br>Variable names can only contain numbers,$ and -For example $my_1stVariable </br> Variables must begin with a letter,- or $. For example $name, _name or name </br>Variable names are case sensitive </br> Variable names should not be JS keywords")
