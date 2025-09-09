6a13b88a52acb5db342d550cfa157af9518eb85c
hunainamalik651@gmail.com <hunainamalik651@gmail.com>
2025-09-09 08:30:12 -0700

    done

---
+++ chap 5/app.js
@@ -0,0 +1,69 @@
+// Question 1 and // Question 2
+let a = 3;
+let b = 5;
+
+console.log(" a = ", a, "& b = ", b);
+// sum of a and b
+console.log(" a + b = ", a + b);
+document.write(" Sum of ", a, " and ", b, " is ", a + b);
+// sub of a and b
+console.log(" a - b = ", a - b);
+document.write("<br> Sub of ", a, " and ", b, " is ", a - b);
+// multiple of a and b
+console.log(" a * b = ", a * b);
+document.write("<br> Multiple of ", a, " and ", b, " is ", a * b);
+//Division  of a and b
+console.log(" a / b = ", a / b);
+document.write("<br> Division of ", a, " and ", b, " is ", a / b);
+// modulus of a and b
+console.log(" a % b = ", a % b);
+document.write("<br>Modulus of ", a, " and ", b, " is ", a % b);
+
+
+// Question 3
+let x;
+document.write("<br><br>Value after variable declaration is ", x);
+x = 5;
+document.write("<br> Initial value : ", x);
+x++;
+document.write("<br> Value after increment is : ", x);
+x= x + 7;
+document.write("<br> Value after addition is : ", x );
+x--;
+document.write("<br> Value after increment is : ", x);
+x = x % 3;
+document.write("<br> The remainder is : ", x);
+
+
+// Question 4
+let ticketPrice = 600;
+let totalcost;
+
+totalcost = ticketPrice * 5;
+document.write("<br><br>Total cost to buy a 5 movie tickets is ",totalcost +"PKR");
+
+// Question 5
+let z = 4;
+
+document.write("<br><br>Table of ",z);
+z = 4*1;
+document.write("<br> 4 x 1 = ", z);
+z = 4*2;
+document.write("<br> 4 x 2 = ", z);
+z = 4*3;
+document.write("<br> 4 x 3 = ", z);
+z = 4*4;
+document.write("<br> 4 x 4 = ", z);
+z = 4*5;
+document.write("<br> 4 x 5 = ", z);
+z = 4*6;
+document.write("<br> 4 x 6 = ", z);
+z = 4*7;
+document.write("<br> 4 x 7 = ", z);
+z = 4*8;
+document.write("<br> 4 x 8 = ", z);
+z = 4*9;
+document.write("<br> 4 x 9 = ", z);
+z = 4*10;
+document.write("<br> 4 x 10 = ", z);
+
