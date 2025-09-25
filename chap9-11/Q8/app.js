d9c55cc9f8076dc56a73907f0bdd2139a5b4e411
hunaina27 <hunainamalik651@gmail.com>
2025-09-25 12:11:52 +0500

    done

---
+++ chap9-11/Q8/app.js
@@ -0,0 +1,9 @@
+// Question 8
+
+let userNo = +prompt("Enter your number");
+
+if (userNo % 3 === 0) {
+    alert("Your number is divisible by 3");
+} else {
+    alert("Your number is NOT divisible by 3");
+}
