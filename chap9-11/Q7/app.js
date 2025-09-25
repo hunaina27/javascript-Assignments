2e703f75c899d40ac808bb66a5a0890fde7abcc9
hunaina27 <hunainamalik651@gmail.com>
2025-09-25 11:58:26 +0500

    Q7 done

---
+++ chap9-11/Q7/app.js
@@ -0,0 +1,10 @@
+//Question 7
+
+let secretNo=7;
+let userNo = +prompt("Enter your number")
+if(userNo===secretNo){
+    alert("Bingo!")
+}
+else{
+    alert("Close enough to the correct answer")
+}
