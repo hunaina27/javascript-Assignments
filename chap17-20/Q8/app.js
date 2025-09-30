916ed7cbd9d0ed6206a5341fccf3c912f0b83f9f
hunaina27 <hunainamalik651@gmail.com>
2025-09-30 11:14:47 +0500

    donme

--- chap17-20/Q8/app.js
+++ chap17-20/Q8/app.js
@@ -1,1 +1,10 @@
 // Question 8
+let A = [24, 53, 78, 91, 12]
+let largest = A[0]
+
+for( let i = 1; i<=A.length; i++){
+    if(A[i]> largest){
+        largest = A[i]
+    }
+}
+console.log ("The largest number is: " + largest)
